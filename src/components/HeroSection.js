5cde7b98c003e76514aff28d1846c96aeefbf2b7
HarshTCP1111 <harsh@techconciergepro.com>
2024-08-20 00:48:51 +0530

    herosection updates

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import './HeroSection.css';
+import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
+
   return (
     <div className="hero-section">
       <div className="hero-content">
         <h1>Welcome to <span>Heal Me Fit!</span></h1>
-        <p>A cutting-edge SaaS platform enhancing safety and well-being in the trucking industry through 
-          biometric data analysis. We provide real-time, actionable health insights to mitigate risks and 
-          reduce compliance costs.</p>
-        <a href="#learn-more-section" className="learn-more-button">Learn More</a>
+        <p>
+          A cutting-edge SaaS platform enhancing safety and well-being in the trucking industry 
+          through biometric data analysis. We provide real-time, actionable health insights to 
+          mitigate risks and reduce compliance costs.
+        </p>
+        <Link 
+          to="services" 
+          className="learn-more-button" 
+          smooth={true} 
+          duration={500}
+          style = {{position: 'relative', zIndex: 10, marginTop: 15}}
+        >
+          Learn More
+        </Link>
       </div>
     </div>
   );
-};
+}
 
 export default HeroSection;
